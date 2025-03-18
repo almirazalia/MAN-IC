204b4f898e3960e6fcba1cb334db537853bef2be
almirazalia <almiraazaliarafif905@gmail.com>
2025-03-18 13:30:06 +0700

    first commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+//console.log ("Halo dunia");
+//document.write ("Saya senang sekali");
+
+
+let namaAja = "almira";//camelCase
+console.log(namaAja);
+//console.log(typeof)(namaAja);
+namaAja = "12345"
+console.log(nama);
+//console.log(typeof)(namaAja);
+
+let kotak = null;
+
+//const nama1
+var nama2 = "almira"
+console.log(nama)
+nama = "rafif"
+console.log(nama)
